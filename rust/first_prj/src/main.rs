ea539a843c1d18cf845f83c7aa65f08c12090747
yuichi.nasukawa <nasu0227@gmail.com>
2024-01-04 13:03:18 +0900

    Add FizzBuzz by rust.

--- rust/first_prj/src/main.rs
+++ rust/first_prj/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    for num in 1..=100 {
+        match (num % 3, num % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            (_, _) => println!("{}", num),
+        }
+    }
 }
