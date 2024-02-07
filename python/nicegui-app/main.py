7e0575c2762f8603cea5164629e2d87d6e82a354
yuichi.nasukawa <nasu0227@gmail.com>
2024-02-07 23:55:38 +0900

    Implemented temporary display for TODO app.

--- python/nicegui-app/main.py
+++ python/nicegui-app/main.py
@@ -1,28 +1,38 @@
-#!/usr/bin/env python3
 from nicegui import ui
 
-with ui.header().classes(replace='row items-center') as header:
-    ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flat color=white')
-    with ui.tabs() as tabs:
-        ui.tab('A')
-        ui.tab('B')
-        ui.tab('C')
+todos = ['TODO1', 'TODO2']
+
+with ui.header().classes(replace="row items-center") as header:
+    ui.button(on_click=None, icon="menu").props("flat color=white")
+    ui.label("TODOアプリ").classes('text-4xl font-bold my-4')
 
 with ui.footer(value=False) as footer:
-    ui.label('Footer')
+    ui.label("Footer")
 
-with ui.left_drawer().classes('bg-blue-100') as left_drawer:
-    ui.label('Side menu')
+with ui.row():
+    with ui.page_sticky(position="bottom-right", x_offset=20, y_offset=20):
+        ui.button(on_click=footer.toggle, icon="contact_support").props("fab")
 
-with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
-    ui.button(on_click=footer.toggle, icon='contact_support').props('fab')
+with ui.row():
+    todo_input = ui.input("TODOを入力してください").classes('w-64')
+    ui.button("追加", on_click=lambda: add_todo(todo_input.value))
 
-with ui.tab_panels(tabs, value='A').classes('w-full'):
-    with ui.tab_panel('A'):
-        ui.label('Content of A')
-    with ui.tab_panel('B'):
-        ui.label('Content of B')
-    with ui.tab_panel('C'):
-        ui.label('Content of C')
+with ui.row():
+    ui.label("Todo List").classes('text-2xl font-semibold my-2')
 
+todo_list = ui.column()
+
+for todo in todos:
+    ui.markdown(todo)
+
+def add_todo(todo_text):
+    if todo_text:
+        # 新しいTODOをUIのtodo_listに直接追加
+        ui.markdown(todo_text)
+        # TODOリスト配列にも追加
+        todos.append(todo_text)
+        # 入力フィールドをクリア
+        todo_input.value = ''
+
+# サーバを起動
 ui.run()
