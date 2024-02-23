6ebed5746c557f34d7de16025a88d1d792e06d4e
yuichi.nasukawa <nasu0227@gmail.com>
2024-02-24 06:25:10 +0900

    reset main.rs

--- rust/first_prj/src/main.rs
+++ rust/first_prj/src/main.rs
@@ -1,38 +1,3 @@
-struct Fibonacci {
-    curr: u64,
-    next: u64,
-}
-
-impl Fibonacci {
-    fn new() -> Fibonacci {
-        Fibonacci { curr: 0, next: 1 }
-    }
-}
-
-impl Iterator for Fibonacci {
-    type Item = u64;
-
-    fn next(&mut self) -> Option<u64> {
-        let new_next = self.curr + self.next;
-        self.curr = self.next;
-        self.next = new_next;
-
-        Some(self.curr)
-    }
-}
-
 fn main() {
     println!("Hello, world!");
-    for num in 1..=100 {
-        match (num % 3, num % 5) {
-            (0, 0) => println!("FizzBuzz"),
-            (0, _) => println!("Fizz"),
-            (_, 0) => println!("Buzz"),
-            (_, _) => println!("{}", num),
-        }
-    }
-    let fib = Fibonacci::new();
-    for i in fib.take(10) {
-        println!("{}", i);
-    }
 }
