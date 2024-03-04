100145de8aa1cab0e2b3b21d2a7cfb23c50db05a
yuichi.nasukawa <nasu0227@gmail.com>
2024-03-05 06:38:02 +0900

    polish.

--- rust/projects/functions/src/main.rs
+++ rust/projects/functions/src/main.rs
@@ -2,12 +2,19 @@ fn main() {
     println!("Hello, world!");
     another_function(5);
     print_labeled_measurement(5, 'h');
+    let x = seven();
+
+    println!("The value of x is: {}", x);
 }
 
 fn another_function(x: i32) {
     println!("The value of x is: {}", x); // xの値は{}です
 }
 
-fn print_labeled_measurement(value:i32, unit_label:char){
+fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {}{}", value, unit_label);
+}
+
+fn seven() -> i32 {
+    7 // ;で閉じてないのでこれは式になり、暗黙的に返される
 }
