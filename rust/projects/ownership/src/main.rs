bb4321b2ef2421ffe54a3112f5734d0086aab74b
yuichi.nasukawa <nasu0227@gmail.com>
2024-03-14 06:16:48 +0900

    use String rust code.

--- rust/projects/ownership/src/main.rs
+++ rust/projects/ownership/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
+    
+    println!("{}", s); // これは`hello, world!`と出力する
 }
