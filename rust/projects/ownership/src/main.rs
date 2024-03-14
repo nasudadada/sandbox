1f233800b0523cddaf90069cadef55f4f86f9add
yuichi.nasukawa <nasu0227@gmail.com>
2024-03-15 06:31:11 +0900

    Add Rust code to check ownership and scope.

--- rust/projects/ownership/src/main.rs
+++ rust/projects/ownership/src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s = String::from("hello"); // sがスコープに入る
 
-    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
-    
-    println!("{}", s); // これは`hello, world!`と出力する
-}
+    takes_ownership(s); // sの値が関数にムーブされ...
+    // println!("s value is {}", s); // ... ここではもう有効ではないので外すとコンパイルエラー
+
+    let x = 5; // xがスコープに入る
+
+    makes_copy(x); // xも関数にムーブされるが、
+                   // i32はCopyなので、この後にxを使っても
+                   // 大丈夫
+} // ここでxがスコープを抜け、sもスコープを抜ける。ただし、sの値はムーブされているので、何も特別なことは起こらない。
+  //
+
+fn takes_ownership(some_string: String) {
+    // some_stringがスコープに入る。
+    println!("{}", some_string);
+} // ここでsome_stringがスコープを抜け、`drop`が呼ばれる。後ろ盾してたメモリが解放される。
+  //
+
+fn makes_copy(some_integer: i32) {
+    // some_integerがスコープに入る
+    println!("{}", some_integer);
+} // ここでsome_integerがスコープを抜ける。何も特別なことはない。
