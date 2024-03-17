ce4e05f354a598edd8d72ae90de837c93937abb7
yuichi.nasukawa <nasu0227@gmail.com>
2024-03-18 06:15:09 +0900

    polish.

--- rust/projects/ownership/src/main.rs
+++ rust/projects/ownership/src/main.rs
@@ -1,12 +1,22 @@
-fn main() {
-    let s1 = String::from("hello");
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
 
-    let len = calculate_length(&s1);
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
 
-    // '{}'の長さは、{}です
-    println!("The length of '{}' is {}.", s1, len);
+    s.len()
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+                               // wordの中身は、値5になる
+
+    s.clear(); // this empties the String, making it equal to ""
+               // Stringを空にする。つまり、""と等しくする
+               // wordはまだもともとのsの最初の単語の文字列長値5を保持しているが、sが空にされてるのでを正しい意味で使用できる文字列は存在しない。
 }
