fc857462fea01e2f3ce724dd85be41a6b79fa08f
yuichi.nasukawa <nasu0227@gmail.com>
2024-03-21 07:38:30 +0900

    polish.

--- rust/projects/structure/src/main.rs
+++ rust/projects/structure/src/main.rs
@@ -5,6 +5,10 @@ struct User {
     active: bool,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+
 fn main() {
     let user1 = User{
         username: String::from("somethinger"),
@@ -13,4 +17,8 @@ fn main() {
         active: true,
     };
     println!("{}", user1.username);
+
+    let black = Color(0, 0, 0);
+    println!("{}", black.0);
+    let origin = Point(0, 0, 0);
 }
